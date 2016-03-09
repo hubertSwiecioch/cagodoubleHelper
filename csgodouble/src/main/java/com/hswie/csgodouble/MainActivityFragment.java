2050596d4abf4a3552d2232b6d5911f7561100df
Hubert Święcioch <Hubert wicioch>
2016-03-10 00:13:06 +0100

    dodanie webinterface#do

--- csgodouble/src/main/java/com/hswie/csgodouble/MainActivityFragment.java
+++ csgodouble/src/main/java/com/hswie/csgodouble/MainActivityFragment.java
@@ -5,18 +5,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
+    private WebView myWebView;
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_main, container, false);
+
+        myWebView = (WebView) v.findViewById(R.id.webview);
+        myWebView.loadUrl("http://www.wp.pl");
+
+        return v;
     }
 }
---
+++ csgodouble/src/main/java/com/hswie/csgodouble/WebAppInterface.java
@@ -0,0 +1,7 @@
+package com.hswie.csgodouble;
+
+/**
+ * Created by hswie on 10.03.2016.
+ */
+public class WebAppInterface {
+}
